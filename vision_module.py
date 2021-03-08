5b2379019250ab94098c897ba9148654fa94ce72
Jesus gonzalez <jesus.gonzalez.xcv@gmail.com>
2021-03-08 11:35:36 -0600

    first iteration

---
+++ vision_module.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def get_image_text(base_64_image):
+    key = 'AIzaSyAHDvJchYHbM84G4Me2HAGkodAQB99C-U0'
+    payload = {'requests': [
+        {
+            'image': {
+                "content": base_64_image
+            },
+            'features': [{'type': 'TEXT_DETECTION'}],
+        },
+    ]}
+
+    r = requests.post(
+        'https://vision.googleapis.com/v1/images:annotate?key=' + key, data=str(payload))
+    res = r.json()
+
+    if 'responses' in res and bool(res['responses'][0]):
+            text = list(res['responses'][0]['textAnnotations'])
+            return list(map(lambda item: item['description'], text))
+    else:
+        return res
+
+
